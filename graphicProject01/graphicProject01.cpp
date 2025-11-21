b9f8b4bae0e1d432c753664ce79f026ceed477ae
mbtoush10 <moustafabtoush079@gmail.com>
2025-11-21 16:04:51 +0300

    dray the wall();

--- graphicProject01/graphicProject01.cpp
+++ graphicProject01/graphicProject01.cpp
@@ -7,12 +7,36 @@ void init() {
 	gluOrtho2D(0, 1280, 0, 720); // Set the orthographic projection to match the window size
 }
 
+void wall() {
+	glColor3ub(153, 102, 51);
+	glBegin(GL_LINES);
+	glVertex2f(0, 100);
+	glVertex2f(1280, 100);
+	glVertex2f(1, 100);
+	glVertex2f(1281, 100);
+	glVertex2f(2, 100);
+	glVertex2f(1282, 100);
+	glVertex2f(3, 100);
+	glVertex2f(1283, 100);
+	glVertex2f(4, 100);
+	glVertex2f(1284, 100);
+	glEnd();
+
+	for (int i = 0; i <= 1280; i += 25) {
+		glColor3ub(153, 102, 51);
+		glBegin(GL_QUADS);
+		glVertex2f(10 + i, 80);
+		glVertex2f(15 + i, 80);
+		glVertex2f(15 + i, 107);
+		glVertex2f(10 + i, 107);
+		glEnd();
+	}
+}
+
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	//////////////////////////////
-	// Your drawing functions here  //
-	//////////////////////////////
+	//wall();
 
 	glFlush();
 
